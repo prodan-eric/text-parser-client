845b5db944f20aa80061c98bd5ec01ffef7d48df
Prodan Eric <ericprodan1920s@gmail.com>
2023-10-07 18:48:47 +0300

    text panel style

--- src/components/UI/TextPanel.tsx
+++ src/components/UI/TextPanel.tsx
@@ -12,7 +12,7 @@ const TextPanel = ({ label, placeholder, value, onChange }: TextPanelProps) => {
     onChange(e.target.value);
   };
   return (
-    <div className="m-4 w-1/2">
+    <div className="m-4 pr-5 md:pr-0 md:w-1/2 w-full">
       <label className="block text-gray-700 text-sm font-bold mb-2">{label}</label>
       <textarea
         className="w-full px-3 py-2 border rounded-md text-gray-700 focus:outline-none focus:border-blue-500 resize-none"
